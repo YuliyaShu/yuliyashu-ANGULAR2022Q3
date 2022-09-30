c5665986e0bb324a9508fc4623a62f5451547269
YuliyaShu <juliette7373@gmail.com>
2022-09-30 10:19:19 +0200

    feat: create cards

--- yourtube-client-app/yourtube/src/app/main/item/item.component.ts
+++ yourtube-client-app/yourtube/src/app/main/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.scss'],
 })
 export class ItemComponent implements OnInit {
+  @Input() videoThumbnail = '';
+  @Input() viewsCount = '';
+  @Input() likesCount = '';
+  @Input() dislikesCount = '';
+  @Input() commentsCount = '';
+  @Input() videoTitle = '';
+
   // constructor() { }
 
   ngOnInit(): void {
--- yourtube-client-app/yourtube/src/app/main/response.service.ts
+++ yourtube-client-app/yourtube/src/app/main/response.service.ts
@@ -1,8 +1,80 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+export interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
+
+export interface ThumbnailsUnit {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Thumbnails {
+    default: ThumbnailsUnit;
+    medium: ThumbnailsUnit;
+    high: ThumbnailsUnit;
+    standard: ThumbnailsUnit;
+    maxres: ThumbnailsUnit;
+}
+
+export interface Localized {
+  title: string;
+  description: string;
+}
+
+export interface Snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  tags: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: Localized;
+  defaultAudioLanguage: string;
+}
+
+export interface Statistics {
+  viewCount: string;
+  likeCount: string;
+  dislikeCount: string;
+  favoriteCount: string;
+  commentCount: string;
+}
+
+export interface Item {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: Snippet;
+  statistics: Statistics;
+}
+
+export interface ItemsList {
+  kind: string;
+  etag: string;
+  pageInfo: PageInfo;
+  items: Item[];
+}
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class ResponseService {
-  // constructor() { }
+  data = '../../response.json';
+
+  itemsList = fetch(this.data).then((resp) => resp.json())
+    .catch((err: Error) => this.openSnackBar(err.message));
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'Undo', { duration: 3000 });
+  }
 }
